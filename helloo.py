63909d302361a54b2753b13ee1a0a4a73c133bd1
Farhana Yesmin <62212985+farhana13@users.noreply.github.com>
2020-03-25 12:18:03 +0200

    Add files via upload
    elevator floor convert

--- helloo.py
+++ helloo.py
@@ -3,3 +3,23 @@ ef = input ('europe floor:')
 usfloor = 1
 uselevator = (int (ef) + usfloor)
 print ('usfloor', uselevator)
+
+
+#if , comparison operators
+x = 5
+if x < 10:
+   print ('smaller')
+if x > 20:
+   print ('bigger')
+print ('done')
+
+#try/except
+rawstr = input ('enter a number: ')
+try:
+    ival = int (rawstr)
+except:
+    ival = -1
+if ival > 0 :
+    print ('nice work')
+else:
+    print ('not a number')
